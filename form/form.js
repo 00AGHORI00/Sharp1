a85f1484f6019e7872daeec309b204ff33b15910
Vedant <vedantvaishnav30@gmail.com>
2023-12-14 08:43:26 +0530

    grouping all the form contents in a folder

--- form/form.js
+++ form/form.js
@@ -10,7 +10,7 @@ myForm.addEventListener('submit', onSubmit);
 function onSubmit(e){
  e.preventDefault();
 
- if(nameInput. value === '' || emailInput.value === ''){
+ if(nameInput.value === '' || emailInput.value === ''){
     msg.classList.add('error');
     msg.innerHTML = 'Please enter all fields';
  
@@ -22,6 +22,18 @@ function onSubmit(e){
  }
 //setting name input and email in local storage
  localStorage.setItem(nameInput.value, String(emailInput.value));
+
 }
 
+// var myObj = {
+//     Name: nameInput.value,
+//     Email: emailInput.value
+// }
+
+// let myObjString = JSON.stringify(myObj);
+
+// localStorage.setItem("user", myObjString);
+
+// let myObjNonString = JSON.parse(localStorage.getItem("user"));
+// console.log(myObjNonString);
 
